0fd5f6efd7b0e3100c9615f270cad0d6ae1dd32c
David Diaz Montenegro <davidcol123dm@gmail.com>
2025-03-19 20:18:47 -0500

    iniciar hu4

--- src/main/java/com/daviddiazm/housing/category/domain/ports/in/MunicipalityServicePort.java
+++ src/main/java/com/daviddiazm/housing/category/domain/ports/in/MunicipalityServicePort.java
@@ -1,7 +1,9 @@
 package com.daviddiazm.housing.category.domain.ports.in;
 
 import com.daviddiazm.housing.category.domain.models.MunicipalityModel;
+import com.daviddiazm.housing.category.domain.models.PagedResult;
 
 public interface MunicipalityServicePort {
     void saveMunicipality (MunicipalityModel municipalityModel);
+    PagedResult<MunicipalityModel> getPageMunicipalities (int page, int size, boolean orderAsc);
 }
--- src/main/java/com/daviddiazm/housing/category/domain/usecases/MunicipalityUseCase.java
+++ src/main/java/com/daviddiazm/housing/category/domain/usecases/MunicipalityUseCase.java
@@ -4,10 +4,12 @@ import com.daviddiazm.housing.category.domain.exceptions.DepartmentIdNotExist;
 import com.daviddiazm.housing.category.domain.exceptions.NameAlreadyExist;
 import com.daviddiazm.housing.category.domain.models.DepartmentModel;
 import com.daviddiazm.housing.category.domain.models.MunicipalityModel;
+import com.daviddiazm.housing.category.domain.models.PagedResult;
 import com.daviddiazm.housing.category.domain.ports.in.MunicipalityServicePort;
 import com.daviddiazm.housing.category.domain.ports.out.DepartmentPersistencePort;
 import com.daviddiazm.housing.category.domain.ports.out.MunicipalityPersistencePort;
 import com.daviddiazm.housing.category.domain.utils.constants.MunicipalityConstants;
+import com.daviddiazm.housing.category.domain.utils.validations.MunicipalityValidator;
 
 import java.util.List;
 
@@ -30,12 +32,20 @@ public class MunicipalityUseCase implements MunicipalityServicePort {
         municipalityModel.setName(municipalityModel.getName().toLowerCase());
         municipalityModel.setDescription(municipalityModel.getDescription().toLowerCase());
 
+        MunicipalityValidator.validateName(municipalityModel.getName());
+        MunicipalityValidator.validateDescription(municipalityModel.getDescription());
+
         MunicipalityModel municipalityExist = getMunicipalityModel(municipalityModel.getName(), department);
         if(municipalityExist != null) {
             throw new NameAlreadyExist(MunicipalityConstants.NAME_ALREADY_EXIST);
         }
 
         municipalityPersistencePort.saveMunicipality(municipalityModel);
+    }
+
+    @Override
+    public PagedResult<MunicipalityModel> getPageMunicipalities(int page, int size, boolean orderAsc) {
+        return null;
     }
 
     private static MunicipalityModel getMunicipalityModel(String nameMunicipality, DepartmentModel department) {
